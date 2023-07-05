4b0d2cdf057f611cffac28bdcac5b5b8a1ee0964
Pachimanok <priopelliza@gmail.com>
2023-07-05 11:43:34 -0300

    lo que hicimos en el curso

---
+++ app/Http/Controllers/productsController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\product;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class productsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $products = product::all();
+        return view('crud.index')->with('productos',$products);
+    
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        
+        return view('crud.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        
+        $product = new product();
+        $product->name = $request['name'];
+        $product->price = $request['price'];
+        $product->description = $request['description'];
+        if($request['activo'] == 'on'){
+
+            $product->activo= 1;
+
+        }else{
+            $product->activo= 0;
+
+        }
+
+        $product->save();
+
+
+        $products = product::all();
+        return view('crud.index')->with('productos',$products);
+    
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        $product = product::find($id);
+        return view('crud.edit')->with('producto',$product);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $pro = product::find($id);
+        $pro->name = $request['name'];
+        $pro->description = $request['description'];
+        $pro->save();
+
+       
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        product::deleted($id);
+    }
+}
---
+++ resources/views/crud/index.blade.php
@@ -0,0 +1,51 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>INDEX - BOOTSTRAP</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+  </head>
+  <body>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-10 mx-auto">
+                <a href="" class="btn-btn-succes">Crear Producto</a>
+                <table class="table">
+                    <thead>
+                        <th>ID</th>
+                        <th>Nombre</th>
+                        <th>Descripcion</th>
+                        <th>Precio</th>
+                        <th>Vendedor</th>
+                        <th>Opciones</th>
+        
+                    </thead>
+                    <tbody>
+                        @foreach ($productos as $producto)
+        
+                            <td>{{ $producto->id }}</td>
+                            <td>{{ $producto->name }}</td>
+                            <td>{{ $producto->description }}</td>
+                            <td>{{ $producto->price }}</td>
+                            <td>{{ $producto->seller }}</td>
+                            <td>
+                                <button class="btn btn-primary">Ed</button>
+                                <button class="btn btn-danger">El</button>
+
+                            </td>
+                            
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+       
+    </div>
+
+
+   
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+  </body>
+</html>
